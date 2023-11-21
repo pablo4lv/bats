bf0ce097503e2ce885a337b958776df9b567a3c5
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-11-21 07:41:51 -0300

    ip´dns

--- ip-dns.bat
+++ ip-dns.bat
@@ -7,13 +7,11 @@ set /p "interfaz=Ingrese el nombre de la interfaz a editar: "
 set /p "ip=Ingrese el numero de ip sin el ultimo numero: "
 set /p "caja=Ingrese el numero de caja: "
 
-netsh interface ipv4 set address name="!interfaz!" source=static address="!ip!.!caja!" mask="255.255.255.0"
+netsh interface ipv4 set address "!interfaz!" static !ip!.!caja! 255.255.255.0 gateway=!ip!.254 
 
-netsh interface ipv4 set address name="!interfaz!" gateway="!ip!.254"
+netsh interface ipv4 set dns "!interfaz!" static "10.50.9.50"
 
-netsh interface ipv4 set dns name="!interfaz!" source=static address="10.50.9.50"
-
-netsh interface ipv4 add dnsserver name="!interfaz!" address="10.50.9.51" index=2
+netsh interface ipv4 add dns "!interfaz!" "10.50.9.51" index=2
 
 ipconfig /all
 
